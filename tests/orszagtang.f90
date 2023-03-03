8c36ff6247197b568223685e28f3e0e1d1512a8c
Ryosuke Hirai <rhirai@localhost.localdomain>
2023-03-03 19:22:48 +1100

    Update: fixed test to work on current HORMONE

--- tests/orszagtang.f90
+++ tests/orszagtang.f90
@@ -10,6 +10,7 @@ subroutine orszagtang
 
   use grid
   use physval
+  use constants,only:pi
 
   implicit none
   
@@ -20,12 +21,14 @@ subroutine orszagtang
 
   do j = js,je
    do i = is,ie
-    d(i,j,k) = gamma*gamma/(4.d0*pi)
+    d(i,j,k) = gamma**2/(4.d0*pi)
     p(i,j,k) = gamma/(4.d0*pi)
-    v1(i,j,k)= -dsin(2.d0*pi*x2(j))
-    v2(i,j,k)=  dsin(2.d0*pi*x1(i))
-    b1(i,j,k)= -dsin(2.d0*pi*x2(j))/dsqrt(4.d0*pi)
-    b2(i,j,k)=  dsin(4.d0*pi*x1(i))/dsqrt(4.d0*pi)
+    v1(i,j,k)= -sin(2.d0*pi*x2(j))
+    v2(i,j,k)=  sin(2.d0*pi*x1(i))
+    v3(i,j,k)= 0d0
+    b1(i,j,k)= -sin(2.d0*pi*x2(j))/sqrt(4.d0*pi)
+    b2(i,j,k)=  sin(4.d0*pi*x1(i))/sqrt(4.d0*pi)
+    b3(i,j,k)= 0d0
    end do
   end do
 
