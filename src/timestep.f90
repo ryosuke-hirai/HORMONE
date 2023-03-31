df0e7eb42766e9c9533cde11773597d81bce4b0e
Ryosuke Hirai <rhirai@localhost.localdomain>
2023-03-31 20:48:07 +1100

    Enhance: changed the way time steps are calculated

--- src/timestep.f90
+++ src/timestep.f90
@@ -1,61 +1,8 @@
 module timestep_mod
 
+ private:: off
+
 contains
-
-!\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-!
-!                           SUBROUTINE EIGEN
-!
-!\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-! PURPOSE: To calcaulte eigenvalues
-
- subroutine eigen
-
-  use grid
-  use settings,only:eostype
-  use physval
-  use pressure_mod
-
-  implicit none
-
-  real*8 asq, bb1, bb2, bb3, bbsq
-  integer ierr
-!--------------------------------------------------------------------------
-!$omp parallel do private(i,j,k,asq,bbsq,bb1,bb2,bb3)
-  do k = ks,ke
-   do j = js,je
-    do i = is,ie
-
-     select case (eostype)
-     case(0:1) ! without recombination
-      call eos_p_cf(d(i,j,k), b1(i,j,k), b2(i,j,k), b3(i,j,k), &
-                    eint(i,j,k), T(i,j,k), imu(i,j,k), p(i,j,k), cf(i,j,k),&
-                    ierr=ierr )
-     case(2) ! with recombination
-      call eos_p_cf(d(i,j,k), b1(i,j,k), b2(i,j,k), b3(i,j,k), &
-                    eint(i,j,k), T(i,j,k), imu(i,j,k), p(i,j,k), cf(i,j,k),&
-                    spc(1,i,j,k), spc(2,i,j,k), ierr )
-     end select
-
-     asq = cf(i,j,k)**2
-
-     bb1  = b1(i,j,k)**2 / d(i,j,k)
-     bb2  = b2(i,j,k)**2 / d(i,j,k)
-     bb3  = b3(i,j,k)**2 / d(i,j,k)
-     bbsq = bb1 + bb2 + bb3
-
-     cf(i,j,k) = asq + bbsq + &
-          sqrt( (asq+bbsq)**2 - 4d0*asq*bb1 )
-     cf(i,j,k) = sqrt( 0.5d0*cf(i,j,k) )
-    end do
-   end do
-  end do
-!$omp end parallel do
- 
-  return
- end subroutine eigen
-
 
 !\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 !
@@ -71,36 +18,62 @@ contains
   use grid
   use physval
   use constants,only:huge
+  use pressure_mod,only:eos_p_cs,get_cf
 
   implicit none
 
-  real*8,allocatable:: dtdist(:,:,:,:)
-  real*8 cfmax
+  real*8,allocatable:: dtdist(:,:,:,:), dti(:,:,:)
+  real*8:: cfmax, cf1,cf2,cf3
+  integer:: ierr
   
 !-------------------------------------------------------------------------
 
-  call eigen
 
-  allocate( dtdist(is:ie,js:je,ks:ke,1:3) )
+  allocate( dtdist(is:ie,js:je,ks:ke,1:3), dti(is:ie,js:je,ks:ke) )
   dtdist = huge
-!$omp parallel do private(i,j,k)
+!!$!$omp parallel do private(i,j,k)
+!!$  do k = ks, ke
+!!$   do j = js, je
+!!$    do i = is, ie
+!!$     if(ie>1)then
+!!$      dtdist(i,j,k,1) = dxi1(i) / ( abs(v1(i,j,k))+abs(v3(i,j,k))+cf(i,j,k) )
+!!$     end if
+!!$     if(je>1)then
+!!$      dtdist(i,j,k,2) = g22(i)*dxi2(j) &
+!!$                      / ( abs(v2(i,j,k))+abs(v3(i,j,k))+cf(i,j,k) )
+!!$     end if
+!!$     if(ke>1)then
+!!$      dtdist(i,j,k,3) = g33(i,j)*dxi3(k) &
+!!$                      / ( abs(v3(i,j,k)+cf(i,j,k) ) )
+!!$     end if
+!!$    end do
+!!$   end do
+!!$  end do
+!!$!$omp end parallel do
+
+  dti = huge
+  cfmax = 0d0
+!$omp parallel do private(i,j,k,cf1,cf2,cf3)
   do k = ks, ke
    do j = js, je
     do i = is, ie
-     dtdist(i,j,k,1) = dxi1(i) / ( abs(v1(i,j,k))+abs(v3(i,j,k))+cf(i,j,k) )
-     if(je>1)then
-      dtdist(i,j,k,2) = g22(i)*dxi2(j) &
-                      / ( abs(v2(i,j,k))+abs(v3(i,j,k))+cf(i,j,k) )
-     end if
-     if(ke>1)then
-      dtdist(i,j,k,3) = g33(i,j)*dxi3(k) &
-                      / ( abs(v3(i,j,k)+cf(i,j,k) ) )
-     end if
+     call eos_p_cs(d(i,j,k), eint(i,j,k), T(i,j,k), imu(i,j,k), &
+                   p(i,j,k), cs(i,j,k), ierr=ierr )
+     cf1 = get_cf(d(i,j,k),cs(i,j,k),b1(i,j,k),b2(i,j,k),b3(i,j,k))
+     cf2 = get_cf(d(i,j,k),cs(i,j,k),b2(i,j,k),b1(i,j,k),b3(i,j,k))
+     cf3 = get_cf(d(i,j,k),cs(i,j,k),b3(i,j,k),b2(i,j,k),b1(i,j,k))
+
+     dti(i,j,k) = dvol(i,j,k) / &
+                ( ( cf1 + abs(v1(i,j,k)) )*off(is,ie) * sa1(i,j,k) &
+                + ( cf2 + abs(v2(i,j,k)) )*off(js,je) * sa2(i,j,k) &
+                + ( cf3 + abs(v3(i,j,k)) )*off(ks,ke) * sa3(i,j,k) )
+
+     cfmax = max(cfmax,cf1,cf2,cf3)
     end do
    end do
   end do
 !$omp end parallel do
-  
+
 ! Temporary (for workaround mesh)
   if(sphrn>0.and.crdnt==2)then
    do i = is, is+2
@@ -128,9 +101,10 @@ contains
    end do
   end if
 
-  dt = minval( dtdist(is:ie,js:je,ks:ke,1:3) )
+!  dt = minval( dtdist(is:ie,js:je,ks:ke,1:3) )
+  dt = minval( dti(is:ie,js:je,ks:ke) )
 
-  cfmax = maxval(abs(cf))
+!  cfmax = maxval(abs(cf))
 
   dt = courant * dt
 !print *,dt,minloc(dtdist(is:ie,js:je,ks:ke,1:3));stop
@@ -140,5 +114,15 @@ contains
 
  return
  end subroutine timestep
+
+ pure function off(is,ie)
+  integer,intent(in)::is,ie
+  real*8:: off
+  if(ie==is)then
+   off = 0d0
+  else
+   off = 1d0
+  end if
+ end function off
 
 end module timestep_mod
