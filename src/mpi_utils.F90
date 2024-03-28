8d3361afef09082b0185ffeab9d79ca2e2ed10af
David Liptai <dliptai@swin.edu.au>
2024-03-28 16:45:53 +1100

    compile with mpi [skip ci]

---
+++ src/mpi_utils.F90
@@ -0,0 +1,26 @@
+module mpi_utils
+#ifdef MPI
+  use mpi
+#endif
+  implicit none
+
+  integer :: ierr
+  integer :: myrank, nprocs
+
+  contains
+
+  subroutine init_mpi()
+#ifdef MPI
+    call MPI_INIT(ierr)
+    call MPI_COMM_RANK(MPI_COMM_WORLD, myrank, ierr)
+    call MPI_COMM_SIZE(MPI_COMM_WORLD, nprocs, ierr)
+#endif
+  end subroutine init_mpi
+
+  subroutine finalize_mpi()
+#ifdef MPI
+    call MPI_FINALIZE(ierr)
+#endif
+  end subroutine finalize_mpi
+
+end module mpi_utils
