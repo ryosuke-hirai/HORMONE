b9d1a53c2ccf680448964f0345c8401564f783f1
Conrad <conradchan@hotmail.com.au>
2024-06-12 17:24:18 +1000

    trigger compiler warning for testing

--- src/mpi_utils.F90
+++ src/mpi_utils.F90
@@ -57,14 +57,6 @@ module mpi_utils
 
     call MPI_ALLREDUCE(value, value_reduced, 1, MPI_REAL8, op, MPI_COMM_WORLD, ierr)
     value = value_reduced
-#else
-    ! Check if operation is valid, but do nothing
-    select case (op_str)
-    case ('sum', 'max', 'min')
-      value = value
-    case default
-      error stop "Invalid operation specified"
-    end select
 #endif
 
   end subroutine allreduce_mpi_real8
