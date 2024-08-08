9af77b0bb759ac0e5517d4f80e2dfb3ff304a018
Ryosuke Hirai <rhirai@localhost.localdomain>
2024-08-08 18:56:42 +1000

    ifx cannot run MPI

--- work/ozstar_mpi.sh
+++ work/ozstar_mpi.sh
@@ -6,7 +6,6 @@
 #SBATCH --cpus-per-task=32
 #SBATCH --time=0:10:00
 #SBATCH --mem-per-cpu=20G
-module load intel-compilers/2023.0.0
 module load gcccore/12.2.0
 export OMP_STACKSIZE=512M
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
