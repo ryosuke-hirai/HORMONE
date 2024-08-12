65d07d6cd5dd14642c4325608557fc6232e62861
Ryosuke Hirai <rhirai@localhost.localdomain>
2024-08-13 02:35:09 +1000

    fix mpi batch script

--- work/ozstar_mpi.sh
+++ work/ozstar_mpi.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #
-#SBATCH --job-name=test
+#SBATCH --job-name=test_mpi
 #
 #SBATCH --ntasks=4
 #SBATCH --cpus-per-task=32
 #SBATCH --time=0:10:00
-#SBATCH --mem-per-cpu=20G
-module load gcccore/12.2.0
+#SBATCH --mem-per-cpu=10G
+module load gcc/12.3.0
+module load openmpi/4.1.5
 export OMP_STACKSIZE=512M
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 OMP_PLACES=cores
 OMP_PROC_BIND=close
-module load openmpi/4.1.5
 srun ./hormone
