dde608f00b5bc7f460795d7428efbad5adf5cd04
Ryosuke Hirai <rhirai@localhost.localdomain>
2024-08-16 03:16:21 +1000

    minor change

--- src/output.f90
+++ src/output.f90
@@ -19,7 +19,7 @@ module output_mod
 
 subroutine output
 
- use settings,only:is_test,in_loop
+ use settings,only:is_test
  use grid,only:tn
  use profiler_mod
  use mpi_utils,only:barrier_mpi
@@ -30,7 +30,7 @@ subroutine output
 
  if(is_test) return ! do not bother outputting anything if it is a test
 
- if(.not.in_loop)then
+ if(tn==0)then
   wtind = wtou1 ! Record time of initial output separately
  else
   wtind = wtout
