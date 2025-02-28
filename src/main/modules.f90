96540686ee7cb8aeff291b5fb4cea2899256fe07
ryosuke-hirai <ryosuke.lufc@gmail.com>
2025-02-28 16:18:23 +1100

    updated some physical constants

--- src/main/modules.f90
+++ src/main/modules.f90
@@ -82,16 +82,20 @@ module constants
   implicit none
 
 ! all in cgs units
-  real(8),parameter:: pi = acos(-1d0)
-  real(8),parameter:: G  = 6.67428d-8, clight = 2.99792458d10
-  real(8),parameter:: msun = 1.989d33, rsun = 6.963d10
-  real(8),parameter:: msolar = msun, rsolar = rsun, au = 1.49598073d13
-  real(8),parameter:: kbol = 1.38064852d-16, amu = 1.6605402d-24
-  real(8),parameter:: arad = 7.5646d-15, sigma = 5.67051d-5
-  real(8),parameter:: hplanck = 6.6260755d-27, m_p = 1.6726231d-24
+
+! Exact values
+  real(8),parameter:: pi = acos(-1d0), clight = 2.99792458d10
+  real(8),parameter:: kbol = 1.380649d-16, hplanck = 6.62607015d-27
+  real(8),parameter:: sigma=2d0*pi**5*kbol**4/(15d0*clight**2*hplanck**3)
+  real(8),parameter:: arad = 4d0*sigma/clight
+! Measured values
+  real(8),parameter:: G  = 6.67428d-8
+  real(8),parameter:: amu = 1.6605402d-24, m_p = 1.6726231d-24
   real(8),parameter:: m_n = 1.6749286d-24, m_e = 9.1093897d-28
   real(8),parameter:: N_A = 6.0221367d23
   real(8),parameter:: year = 3600d0*24d0*365.25d0
+  real(8),parameter:: msun = 1.989d33, rsun = 6.963d10
+  real(8),parameter:: msolar = msun, rsolar = rsun, au = 1.49598073d13
   real(8),parameter:: huge = 1d99, tiny = 1d-99
   real(8):: fac_pgas, fac_egas
 
