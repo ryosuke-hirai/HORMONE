43aa75e39e2542d98d25f4c9dc080c881f3e9897
Afiq <muhammadafiq4222@gmail.com>
2025-11-28 18:54:19 +0800

    Updated horpy to be able to output frame and dvols

--- scripts/horpy.sh
+++ scripts/horpy.sh
@@ -101,7 +101,7 @@ cd "$PROJECT_ROOT/src/main"
 $F2PY -c "$SCRIPT_DIR/horpy.pyf" -m horpy \
     --fcompiler=gfortran --f90flags='-fopenmp' -lgomp \
     mpi_utils.F90 derived_types.f90 modules.f90 profiler.f90 \
-    mpi_domain.F90 io.F90 utils.f90 conserve.f90 ionization.f90 \
+    mpi_domain.F90 io.F90 utils.f90 metric.f90 conserve.f90 ionization.f90 \
     eos.f90 matrix_vars.F90 opacity.f90 radiation_utils.f90 matrix_coeffs.f90 \
     matrix_utils.f90 miccg.f90 fluxlimiter.f90 hlldflux.f90 dirichlet.f90 \
     particles.f90 petsc_solver.F90 matrix_solver.F90 radiation.f90 cooling.f90 \
